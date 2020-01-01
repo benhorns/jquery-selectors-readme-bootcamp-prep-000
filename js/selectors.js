519a01f9b178be027d0212b1c1144b673d6663f1
Benjamin Horns <benhorns@gmail.com>
2020-01-01 21:05:15 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,12 +1,4 @@
 // declare your functions here...
-$('img')
-$('.pics')
-$('#baby-ninja')
-$('ul li')
-$('parent-tag child-tag:first-child')
-$('div img:first-child')
-$("img[alt='the beatles makingfaces']")
-$('div:last')
 
 function paragraphSelector(){
   return $('p')
